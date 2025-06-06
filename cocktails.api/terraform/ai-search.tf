3a302d7c31977b1045f6b4e1560ac474c7ec1b22
Ronaldo Vecchi <rvecchi@gmail.com>
2025-06-05 20:13:48 -0600

    updates

--- cocktails.api/terraform/ai-search.tf
+++ cocktails.api/terraform/ai-search.tf
@@ -4,7 +4,7 @@ module "ai_search_cosmos_index" {
   ai_search_service_key       = data.azurerm_search_service.ai_search_service.primary_key
   search_service_principal_id = data.azurerm_search_service.ai_search_service.identity[0].principal_id
   cosmosdb_account_id         = module.cocktails_cosmosdb_account.cosmosdb_account_id
-  cosmos_database_name        = var.cocktails_cosmosdb_database_name
+  cosmos_database_name        = module.cocktails_cosmosdb_database.database_name
   cosmos_container_name       = "cocktails-cocktail"
   tags                        = local.tags
 }
