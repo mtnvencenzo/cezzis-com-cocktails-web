6f8f21d52768139dd2a2e39c27980e34469210e6
Ronaldo Vecchi <rvecchi@gmail.com>
2025-06-05 19:53:04 -0600

    test(ai searhc index) Removing index (for real this time)

--- cocktails.api/terraform/ai-search.tf
+++ cocktails.api/terraform/ai-search.tf
@@ -1,14 +1,14 @@
-# module "ai_search_cosmos_index" {
-#   source                      = "./ai_search_index"
-#   ai_search_service_host_name = "srch-${var.sub}-${var.region}-${var.global_environment}-${var.global_domain}-${var.sequence}"
-#   ai_search_service_key       = data.azurerm_search_service.ai_search_service.primary_key
-#   search_service_principal_id = data.azurerm_search_service.ai_search_service.identity[0].principal_id
-#   cosmosdb_account_id         = module.cocktails_cosmosdb_account.cosmosdb_account_id
-#   cosmos_database_name        = var.cocktails_cosmosdb_database_name
-#   cosmos_container_name       = "cocktails-cocktail"
-#   tags                        = local.tags
+module "ai_search_cosmos_index" {
+  source                      = "./ai_search_index"
+  ai_search_service_host_name = "srch-${var.sub}-${var.region}-${var.global_environment}-${var.global_domain}-${var.sequence}"
+  ai_search_service_key       = data.azurerm_search_service.ai_search_service.primary_key
+  search_service_principal_id = data.azurerm_search_service.ai_search_service.identity[0].principal_id
+  cosmosdb_account_id         = module.cocktails_cosmosdb_account.cosmosdb_account_id
+  cosmos_database_name        = var.cocktails_cosmosdb_database_name
+  cosmos_container_name       = "cocktails-cocktail"
+  tags                        = local.tags
 
-#   depends_on = [
-#     module.cocktails_cosmosdb_database
-#   ]
-# }
+  depends_on = [
+    module.cocktails_cosmosdb_database
+  ]
+}
