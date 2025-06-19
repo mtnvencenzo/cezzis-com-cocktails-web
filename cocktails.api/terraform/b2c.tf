6f42a4d724079b80de0cca15108e089bdbe0d57e
Ronaldo Vecchi <rvecchi@gmail.com>
2025-06-19 15:40:07 -0600

    Updates to associate domain name

--- cocktails.api/terraform/b2c.tf
+++ cocktails.api/terraform/b2c.tf
@@ -8,12 +8,12 @@ module "api_b2c_tenant" {
   sub                = var.sub
   sequence           = var.sequence
   b2c_tenant_name    = var.b2c_tenant_name
-  dns_zone_name      = data.azurerm_dns_zone.cezzis_dns_zone.name
 
   login_subdomain          = var.login_subdomain
   cdn_frontdoor_profile_id = data.azurerm_cdn_frontdoor_profile.global_shared_cdn.id
   dns_zone_id              = data.azurerm_dns_zone.cezzis_dns_zone.id
   dns_zone_resource_group  = data.azurerm_resource_group.cocktails_global_resource_group.name
+  dns_zone_name            = data.azurerm_dns_zone.cezzis_dns_zone.name
   allowed_origins          = var.allowed_origins
 
   tags = local.tags
@@ -22,4 +22,10 @@ module "api_b2c_tenant" {
     azuread = azuread
     azurerm = azurerm
   }
+}
+
+resource "azurerm_active_directory_domain_name" "custom_b2c_login_domain" {
+  domain_name         = "${var.login_subdomain}.${dns_zone_name}"
+  resource_group_name = data.azurerm_resource_group.cocktails_resource_group.name
+  azure_b2c_tenant_id = var.b2c_tenant_id
 }
