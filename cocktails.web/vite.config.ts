f479d5ae6b5eb7ce938f832a325929de310e3520
mtnvencenzo <rvecchi@gmail.com>
2025-10-03 19:56:28 -0600

    updates

--- cocktails.web/vite.config.ts
+++ cocktails.web/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import mkcert from 'vite-plugin-mkcert';
+// eslint-disable-next-line import/no-unresolved
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
