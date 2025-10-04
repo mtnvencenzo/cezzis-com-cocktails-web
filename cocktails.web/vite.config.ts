9d4b044407319889ffd1fa04187d581cbcd92664
mtnvencenzo <rvecchi@gmail.com>
2025-10-03 19:56:01 -0600

    fix

--- cocktails.web/vite.config.ts
+++ cocktails.web/vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig, loadEnv } from 'vite';
 import mkcert from 'vite-plugin-mkcert';
-import tsconfigPaths from 'vite-tsconfig-paths';
-// eslint-disable-next-line import/no-unresolved
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default ({ mode }) => {
     process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
@@ -11,10 +10,27 @@ export default ({ mode }) => {
         base: '/',
         build: {
             target: 'esnext',
-            minify: 'esbuild'
+            rollupOptions: {
+                output: {
+                    manualChunks(id) {
+                        // 1. Group all Node.js modules into a 'vendor' chunk
+                        if (id.includes('node_modules')) {
+                            return 'vendor'; // Default vendor chunk for other node_modules
+                        }
+
+                        // 5. Default behavior: let Rollup handle remaining files
+                        // Returning `undefined` or nothing allows Rollup's default chunking strategy
+                        // to take over for files not explicitly assigned to a manual chunk.
+                        return undefined;
+                    }
+                }
+            }
         },
         server: {
             port: parseInt(process.env.VITE_PORT, 10)
+        },
+        resolve: {
+            conditions: ['mui-modern', 'module', 'browser', 'local|development|production']
         },
         plugins: [react(), tsconfigPaths(), mkcert()]
     });
